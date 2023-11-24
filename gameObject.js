18bbbbcf482ba4dd3946c6631208aa00a52b5343
Sergio Venegas <69369902+Angcroft@users.noreply.github.com>
2023-11-24 09:39:39 -0300

    Create gameObject.js

---
+++ gameObject.js
@@ -0,0 +1,15 @@
+//    Clase común para los objetos bullet y enemigos
+class GameObject {
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    show() {
+        fill(this.color);
+        rect(this.x, this.y, this.width, this.height);
+    }
+}
